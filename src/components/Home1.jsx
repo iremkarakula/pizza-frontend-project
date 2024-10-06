f7e8208ac1209da97fc42673f0fc821d8f2b22e6
iremkarakula <irem123458@gmail.com>
2024-10-06 15:47:13 +0300

    home1 tamamlandı

---
+++ src/components/Home1.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Home1.css'
+
+function Home1() {
+    return (
+        <div className='home1'>
+            <img src='../Assets/Iteration-1-assets/home-banner.png' className='home1-img' />
+            <div className='home1-text'>
+                <h1 className='home1-h1'>Teknolojik Yemekler</h1>
+                <h2 className='home1-h2'>KOD ACIKTIRIR <br /> PİZZA, DOYURUR</h2>
+                <button className='home1-button'>ACIKTIM</button>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default Home1
---
+++ src/components/Iteration1.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Home1 from './Home1.jsx'
+import './Iteration1.css'
+
+function Iteration1() {
+    return (
+        <div className='iteration1'>
+            <Home1 />
+        </div>
+    )
+}
+
+export default Iteration1
